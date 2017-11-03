0a29d908bf9291540a1099a5c4aeb68b5264aca5
BrianLim167 <blim167@gmail.com>
2017-11-03 02:39:50 -0400

    finished w09

--- dirinfo.c
+++ dirinfo.c
@@ -2,17 +2,45 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <sys/types.h>
-
+#include <sys/stat.h>
 
 int main(){
-  DIR *dir = opendir("./");
-  struct dirent *ent;
+  char path[] = "./";
 
-  while (ent = readdir(dir)){
-    printf("%s\n", ent->d_name);
+  DIR *siz = opendir(path);
+  DIR *dir = opendir(path);
+  DIR *fil = opendir(path);
+  struct dirent *ent;
+  struct stat buffer;
+  size_t size = 0;
+
+  printf("Stats for %s\n",path);
+  printf("========\n");
+
+  printf("Size\n");
+  while (ent = readdir(siz)){
+    stat(ent->d_name,&buffer);
+    if (ent->d_type == DT_REG) size += buffer.st_size;
   }
-  
+  printf("\t%lu bytes\n", size);
+  printf("--------\n");
+
+  printf("Directories\n");
+  while (ent = readdir(dir)){
+    if (ent->d_type == DT_DIR) printf("\t%s\n", ent->d_name);
+  }
+  printf("--------\n");
+
+  printf("Files\n");
+  while (ent = readdir(fil)){
+    if (ent->d_type == DT_REG) printf("\t%s\n", ent->d_name);
+  }
+  printf("--------\n");
+
+
+  closedir(siz);
   closedir(dir);
-  
+  closedir(fil);
+
   return 0;
 }
